10498d97cd08461d848b6c1be4c72379625bdbfb
SafiraRahmaMaulidah <112893636+SafiraRahmaMaulidah@users.noreply.github.com>
2022-09-19 15:23:42 +0700

    Add files via upload

---
+++ tugas pertemuan 3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    string nama;
+    cout << "Tuliskan Nama ; ";
+    cin >> nama;
+    cout << "Hi gaesss ";
+    cout << "welcome to my codeblock" << endl;
+
+    int u = 19;
+    float t = 163.5;
+    char b = 'XXL';
+    string ub = "XXL";
+
+    cout << "Umur saya : " << u;
+    cout << "\nTinggi saya : " << t;
+    cout << "\nUkuran baju : " << ub;
+    return 0;
+}
